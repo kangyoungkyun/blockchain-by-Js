4eedd838b70dcf9f6404bff2fdfe89954c8b0ed4
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-25 15:08:55 +0900

    새로운 블락 과 새로운 트랜잭션 생성 - (총금액, 보내는이, 받는이)

--- dev/test.js
+++ dev/test.js
@@ -6,11 +6,31 @@ const bitcoin = new Blockchain();
 
 //새로운 블락 만들기
 bitcoin.createNewBlock(1111,"aaaaaaa","1a1a1a1a1a1a");
-bitcoin.createNewBlock(2222,"bbbbbbb","2b2b2b2b2b2b");
-bitcoin.createNewBlock(3333,"ccccccc","3c3c3c3c3c3c");
+
+//새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
+bitcoin.createNewTransaction(100,'PACKadffaaf','HONGllalflks') 
 
 //찍어보기
 console.log(bitcoin)
 
+
 //결과
-//Blockchain { chain: [], newTransactions: [] }
+/*
+Blockchain {
+    chain: 
+     [ { index: 1,
+         timestamp: 1527228481937,
+         transactions: [],
+         nonce: 1111,
+         hash: '1a1a1a1a1a1a',
+         previousBlockHash: 'aaaaaaa' } ],
+    pendingTransaction: 
+     [ { amount: 100, sender: 'PACKadffaaf', recipient: 'HONGllalflks' } ] }
+*/
+
+
+
+
+
+
+
