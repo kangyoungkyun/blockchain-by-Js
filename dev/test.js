3a1eef94067214c802d5f0560eb5d8745e1861a7
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-25 13:47:49 +0900

    새로운 블락 만들기

--- dev/test.js
+++ dev/test.js
@@ -4,5 +4,11 @@ const Blockchain = require('./blockchain')
 //위에서 가져온 모듈의 객체를 만든다.
 const bitcoin = new Blockchain();
 
+//새로운 블락 만들기
+bitcoin.createNewBlock(1111,"aaaaaaa","1a1a1a1a1a1a");
+
 //찍어보기
 console.log(bitcoin)
+
+//결과
+//Blockchain { chain: [], newTransactions: [] }
