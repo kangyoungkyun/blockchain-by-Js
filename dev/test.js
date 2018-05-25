ac0a74e338af5b93bedfd9486c7a9cabaa17f3dc
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-25 15:20:14 +0900

    새로운 블락 생성 - 채굴후 얻은 블락 - 마이닝 후 트랜젝션 데이터의 변화

--- dev/test.js
+++ dev/test.js
@@ -10,6 +10,10 @@ bitcoin.createNewBlock(1111,"aaaaaaa","1a1a1a1a1a1a");
 //새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
 bitcoin.createNewTransaction(100,'PACKadffaaf','HONGllalflks') 
 
+
+//새로운 블락 생성 - 채굴후 얻은 블락
+bitcoin.createNewBlock(2222,"bbbbbbb","2b2b2b2b2b2b");
+
 //찍어보기
 console.log(bitcoin)
 
@@ -17,15 +21,21 @@ console.log(bitcoin)
 //결과
 /*
 Blockchain {
-    chain: 
-     [ { index: 1,
-         timestamp: 1527228481937,
-         transactions: [],
-         nonce: 1111,
-         hash: '1a1a1a1a1a1a',
-         previousBlockHash: 'aaaaaaa' } ],
-    pendingTransaction: 
-     [ { amount: 100, sender: 'PACKadffaaf', recipient: 'HONGllalflks' } ] }
+  chain: 
+   [ { index: 1,
+       timestamp: 1527229062594,
+       transactions: [],
+       nonce: 1111,
+       hash: '1a1a1a1a1a1a',
+       previousBlockHash: 'aaaaaaa' },
+     { index: 2,
+       timestamp: 1527229062594,
+       transactions: [Array],
+       nonce: 2222,
+       hash: '2b2b2b2b2b2b',
+       previousBlockHash: 'bbbbbbb' } ],
+  pendingTransaction: [] }
+
 */
 
 
