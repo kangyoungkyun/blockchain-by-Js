977ec968e12166a1c0c2feb9f9090ee6850edec0
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-25 11:26:40 +0900

    블록체인 데이터 구조 생성하기

---
+++ dev/blockchain.js
@@ -0,0 +1,13 @@
+//블록체인 데이터 구조
+function Blockchain(){
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+//클래스를 이용한 블록체인 데이터 구조 위와 동일하다.
+class Blockchain{
+    constructor(){
+        this.chain = [];
+        this.newTransactions = [];
+    }
+}
