1baa93aadb226f36e210126060c6862adfc3a859
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-28 16:59:27 +0900

    웹브라우저 주소 입력했을 때

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,26 @@
 var express = require('express')
 var app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+// 웹브라우저에 get 방식으로 /blockchain 주소를 입력했을 때 실행
+app.get('/blockchain', function (req, res) {
+  res.send('blockchain Start')
 })
  
-app.listen(3000)
+
+// 웹브라우저에 post 방식으로 /transaction 주소를 입력했을 때 실행
+app.post('/transaction', function (req, res) {
+  res.send('transaction Start')
+})
+
+
+// 웹브라우저에 get 방식으로 /mine 주소를 입력했을 때 실행
+app.get('/mine', function (req, res) {
+  res.send('mine Start')
+})
+
+
+
+app.listen(3000,function(){
+  console.log('listening on port 3000')
+})
