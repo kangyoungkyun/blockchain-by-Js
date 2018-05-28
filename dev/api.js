e5b4f7d5b672c375649cd4d2e94284b74cc8a157
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-28 17:33:01 +0900

    body parser api 설정 완료

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
- 
+//bodyParser 임포트
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 // 웹브라우저에 get 방식으로 /blockchain 주소를 입력했을 때 실행
 app.get('/blockchain', function (req, res) {
@@ -10,7 +14,8 @@ app.get('/blockchain', function (req, res) {
 
 // 웹브라우저에 post 방식으로 /transaction 주소를 입력했을 때 실행
 app.post('/transaction', function (req, res) {
-  res.send('transaction Start')
+  console.log(req.body);
+  res.send(`거래 총액은 ${req.body.amount} 코인`);
 })
 
 
